b99fef4237af50ac010c7bcb9264d4bfe2815709
Your Name <you@example.com>
2025-02-06 11:18:33 -0600

    converted data to CSV

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import conda
 import yfinance as yf
-import statsmodels.tsa.statespace.arimax import ARIMAX
-%matplotlib inline
-data = yf.download("CL=F", start="2010-01-01", end="2020-01-01")
-print(data.head())
+import csv
+data = yf.download("CL=F", start="2010-01-01", end="2024-12-30")
 print(data)
-print("Hello world!")
+
+df = pd.DataFrame(data)
+
+# Convert DataFrame to CSV
+df.to_csv('output.csv', index=True)
+
+# import statsmodels.tsa.statespace.arimax import ARIMAX
+# %matplotlib inline
+# print("Hello world!")
