d41afccaf852238cffe70da9b6b115d79ef8c818
Ali Mostafa <amostafa1@cps.edu>
2025-02-21 11:00:16 -0600

    upd

---
+++ misc/yahoofinancetest.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import yfinance as yf
+import csv
+data = yf.download("CL=F", start="2010-01-01", end="2024-12-30")
+print(data)
+
+df = pd.DataFrame(data)
+
+# Convert DataFrame to CSV
+df.to_csv('output.csv', index=True)
+
+# import statsmodels.tsa.statespace.arimax import ARIMAX
+# %matplotlib inline
+# print("Hello world!")
