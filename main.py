88d54a443bf78c104646570f454ed24591d7b87a
amostafa10 <amostafa1@cps.edu>
2025-03-02 22:24:03 -0600

    price visualization

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv("yahoocrudeoildata.csv", delim_whitespace=True)
+dataframe = pd.read_csv("yahoocrudeoildata.csv")
 
-data['Date'] = pd.to_datetime(data['Date'])
+dataframe['Date'] = pd.to_datetime(dataframe['Date'])
+dataframe = dataframe.set_index('Date')
+
+print(dataframe)
+
+plt.title('Crude Oil Price vs Time')
+plt.xlabel('Time')
+plt.ylabel('Crude Oil Price')
+plt.plot(dataframe.index, dataframe['Close'])
+plt.show()
