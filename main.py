e5932059e19d594c17034ebcd9b93f253394144f
amostafa10 <amostafa1@cps.edu>
2025-03-03 22:24:09 -0600

    arima forecast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.arima.model import ARIMA
@@ -9,12 +10,20 @@ from sklearn.metrics import mean_squared_error
 csv_path = 'yahoocrudeoildata.csv'
 data = pd.read_csv(
     csv_path,
-    parse_dates=['Date'], # Parse the dates
+    parse_dates=['Date'],  # Parse the dates
     date_format='%Y-%m-%d',
-    index_col='Date' # Set the date as the index
+    index_col='Date'  # Set the date as the index
 )
 
 data.sort_index(inplace=True)
+
+# Graph price over time data
+# plt.plot(data.index, data['Close'], label='Close Price')
+# plt.title('Crude Oil Price Over Time')
+# plt.xlabel('Date')
+# plt.ylabel('Crude Oil Price')
+# plt.legend()
+# plt.show()
 
 # Test for stationarity
 adf_test_original = adfuller(data['Close'])
@@ -31,39 +40,70 @@ if pValue_original < 0.05:
 else:
     print('original data is not stationary, differencing needed')
 
-    # Difference our data
-    data['Close_Diff'] = data['Close'].diff()
 
-    # Retest our data
-    adf_test_diff = adfuller(data["Close_Diff"].dropna())
-    adfStatistic_diff = adf_test_diff[0]
-    pValue_diff = adf_test_diff[1]
+# Difference our data
+data['Close_Diff'] = data['Close'].diff()
 
-    print('ADF Statistic (differenced): %f' % adfStatistic_diff)
-    print('p-value (differenced): %f' % pValue_diff)
+# Retest our data
+adf_test_diff = adfuller(data["Close_Diff"].dropna())
+adfStatistic_diff = adf_test_diff[0]
+pValue_diff = adf_test_diff[1]
+
+print('ADF Statistic (differenced): %f' % adfStatistic_diff)
+print('p-value (differenced): %f' % pValue_diff)
 
 # We are assuming our order of differencing is 1, since that's how much I've had to do in testing.
 # Replace with a while-loop to keep differencing and incrementing our order of differencing for a more automated solution.
 
 # Graph differenced data
-plt.figure(figsize=(7, 6))
-plt.plot(data.index, data['Close_Diff'], label='Differenced Close Price')
-plt.title('Differenced Close Price Over Time')
+# plt.figure(figsize=(7, 6))
+# plt.plot(data.index, data['Close_Diff'], label='Differenced Close Price')
+# plt.title('Differenced Close Price Over Time')
+# plt.xlabel('Date')
+# plt.ylabel('Differenced Close Price')
+# plt.legend()
+# plt.show()
+
+# Autocorrelation and Partial Autocorrelation to determine ARIMA parameters.
+# In terms of our differenced data!
+# fig, axes = plt.subplots(1, 2, figsize=(16, 4))
+
+# plot_acf(data['Close_Diff'].dropna(), lags=40, ax=axes[0])
+# axes[0].set_title('Autocorrelation Function (ACF)')
+
+# plot_pacf(data['Close_Diff'].dropna(), lags=40, ax=axes[1])
+# axes[1].set_title('Partial Autocorrelation Function (PACF)')
+
+plt.show()
+
+# See that both the ACF and PACF crosses over at 1, those are our q and p values of our ARIMA model respectively.
+
+# Split our data into testing and training data, we're going to use an 80% / 20% split.
+train_size = int(len(data) * 0.8)
+train, test = data.iloc[:train_size], data.iloc[train_size:]
+
+# Fit ARIMA model
+model = ARIMA(train['Close'], order=(1, 1, 1))
+model_fit = model.fit()
+
+# Test and visualize the results
+forecast = model_fit.forecast(steps=len(test))
+
+plt.figure(figsize=(14, 7))
+plt.plot(train.index, train['Close'], label='Train', color='#203147') # Plot the training data
+plt.plot(test.index, test['Close'], label='Test', color='#01ef63') # Plot the real testing data
+plt.plot(test.index, forecast, label='Forecast', color='orange') # Plot the forecasted data
+plt.title('Close Price Forecast')
 plt.xlabel('Date')
-plt.ylabel('Differenced Close Price')
+plt.ylabel('Close Price')
 plt.legend()
 plt.show()
 
-# Autocorrelation and Partial Autocorrelation to determine ARIMA parameters.
-plot_acf(data['Close'], lags=40)
-plot_pacf(data['Close'], lags=40)
-plt.show()
+print(f'AIC: {model_fit.aic}')
+print(f'BIC: {model_fit.bic}')
 
+forecast = forecast[:len(test)]
+test_close = test['Close'][:len(forecast)]
 
-
-# Graph data
-# plt.title('Crude Oil Price vs Time')
-# plt.xlabel('Time')
-# plt.ylabel('Crude Oil Price')
-# plt.plot(data.index, data['Close'])
-# plt.show()
+rmse = np.sqrt(mean_squared_error(test_close, forecast))
+print(f'RMSE: {rmse:.4f}')
